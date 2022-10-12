9f94d9f666af56886397d4196a9d5425c7459741
marramags <marra.emata@gmail.com>
2022-10-12 09:55:23 -0700

    trying to access giftItems array from schema/form

--- controllers/api/gifts.js
+++ controllers/api/gifts.js
@@ -1,15 +1,25 @@
 const Recipient = require('../../models/recipient');
 
 async function addGift(req, res) {
+
     const giftList = await new Recipient(req.body);
+    // Recipient.giftItems.push( 
+    //     { 
+    //        item: req.body.item,  
+    //        description: req.body.description,
+    //        storeLink: req.body.storeLink,
+    //        bought: req.body.bought
+    //     });
+    
     console.log(`this is the req.body: ${req.body}`)
     giftList.save()
     res.json(giftList)
     console.log(`this is the gift list: ${giftList}`)
     // try{
     //     const addGiftList = await Recipient.create(req.body);
-    //     // addGiftList.save
-    //     console.log(req.body)
+    //     addGiftList.save
+    //     // Recipient.giftItems.push({req.body})
+    //     console.log(addGiftList)
     //     res.json(addGiftList)
     //     console.log(`this is the gift list: ${addGiftList}`)
 
